a140593d34997b1b20dd9f226c8f9441279dfdea
cloudwithrk <cloudwithrk@gmail.com>
2025-07-17 23:21:40 +0530

    Changed in axios file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from './axios';
 import './styles.css';
 
 import { ToastContainer, toast } from 'react-toastify';
